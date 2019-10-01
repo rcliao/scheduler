c3ed3f1e8e12877003bb51ff0c3f9c5e4c0b518c
Eric Liao <rcliao01@gmail.com>
2019-10-01 13:34:05 +0000

    feat: improve overall interface design

--- scheduler.go
+++ scheduler.go
@@ -2,17 +2,35 @@ package scheduler
 
 import "time"
 
-type WorkFn func() error
+// JobType defines different kinds of job to open up different ways of processing
+// the same job
+type JobType int
 
+const (
+	// DebugFunc simply logs the message in console for debugging purpose
+	DebugFunc JobType = iota
+	// WebHook is the most basic job to send the job data to a HTTP endpoint
+	WebHook
+)
+
+// Job is the base entity to store the scheduling meta data as well as its work
 type Job struct {
 	ID                 string
 	ScheduledTimestamp time.Time
 	Status             string
-	Work               WorkFn
+	Type               JobType
+	Data               string
 }
 
+// JobRepository manages the database interaction to store and retrieve job data
+type JobRepository interface {
+	GetJob(ID string) (Job, error)
+	PutJob(ID string, job Job) (Job, error)
+}
+
+// Scheduler is the top level API to schedule a delay job
 type Scheduler interface {
-	DelayJob(milliSeconds int, job WorkFn) (Job, error)
-	Reschedule(ID string, newDelayedDuration time.Time) (Job, error)
+	DelayJob(duration time.Duration, jobType JobType, data string) (Job, error)
+	Reschedule(ID string, duration time.Duration) (Job, error)
 	Cancel(ID string) error
 }
