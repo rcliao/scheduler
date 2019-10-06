bfb174cf96d2b28299b2cba1f5a795daf9541ab2
Eric Liao <rcliao01@gmail.com>
2019-10-06 22:36:46 +0000

    feat: add native scheduler implementation

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"net/http"
+	"time"
 
+	"github.com/rcliao/scheduler"
 	"github.com/rcliao/scheduler/web"
+	"github.com/rcliao/scheduler/worker"
 )
 
 func main() {
+	schedulerInstance := worker.NewNativeScheduler()
+	duration, _ := time.ParseDuration("10s")
+	schedulerInstance.DelayJob(duration, "test", scheduler.DebugFunc)
 	http.HandleFunc("/hello", web.HelloHandler())
 	http.ListenAndServe(":9000", nil)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rcliao/scheduler
 
 go 1.12
+
+require github.com/google/uuid v1.1.1
